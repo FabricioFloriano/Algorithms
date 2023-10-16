3bbb1e953ebe13e87b279268bb94bf446c0e5593
Fabricio Floriano <f.florian0@hotmail.com>
2023-10-16 10:10:04 -0300

    Iniciando tests

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,16 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    message = "fabrici01"
+    invalid_key = "fabricio"
+    invalid_message = 123
+    with pytest.raises(TypeError, match="Invalid type for key"):
+        encrypt_message(message, invalid_key)
+
+    with pytest.raises(TypeError, match="Invalid type for message"):
+        encrypt_message(invalid_message, 123)
+
+
+# Duvidas tirar na mentoria
