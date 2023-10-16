f96b4bacd6ea254d07881af5301e89e2d06e9200
Fabricio Floriano <f.florian0@hotmail.com>
2023-10-16 19:06:00 -0300

    Requisito 05?

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,18 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if len(nums) < 0:
+        return False
+
+    nums.sort()
+    index = 0
+    seen_numbers = set()
+
+    while index < len(nums):
+        num = nums[index]
+        if not isinstance(num, int) or num < 0:
+            return False
+        if num in seen_numbers:
+            return num
+        seen_numbers.add(num)
+        index += 1
+
+    return False
