99b22e7b9444d5f468148e2c101c62dbc325d17b
Fabricio Floriano <f.florian0@hotmail.com>
2023-10-16 09:26:12 -0300

    Requisito 01?

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,14 @@
-def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+def study_schedule(persistence_periods, target_time):
+    if target_time is None:
+        return None
+
+    student_count = 0
+    for start_period, end_period in persistence_periods:
+        if not isinstance(start_period, int) \
+              or not isinstance(end_period, int):
+            return None  
+
+        if start_period <= target_time <= end_period:
+            student_count += 1
+
+    return student_count
